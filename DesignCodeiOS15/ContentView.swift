cfd6a4579c0ed02ecfec430627aed09aae947e67
jydeveloper <lukaryu@naver.com>
2021-11-11 15:53:15 +0900

    Initial Commit

---
+++ DesignCodeiOS15/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCodeiOS15
+//
+//  Created by 유정용 on 2021/11/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by 유정용 on 2021/11/11.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
